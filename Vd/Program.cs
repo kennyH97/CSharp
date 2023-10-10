b728c6e734992f231ba69580ef0bec7aa10b9fa8
kennyH97 <e.kennyh.v@gmail.com>
2023-10-10 11:53:54 +0700

    Add project files.

---
+++ Vd/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Student std = new Student();
+        std.ShowInfo();
+        
+    }
+
+    public class Student
+    {
+        private int id;
+        private string name;
+        public int age;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            private set 
+            {
+                id = value; 
+            }
+        }
+
+        public int Class { get; set; }
+        
+        public void ShowInfo()
+        {
+            Id = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"Name: {name} - age: {age} - Id: {Id} - class: {Class}");
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
